fe7d5e0cb4e34365cf016f5ae1bced221051717d
Aleksei V. Simonov <wert001@yandex.ru>
2025-08-09 21:56:30 +0300

    fix equals and hashCode for CrashReport

--- client/src/main/java/ru/wert/tubus/client/entity/models/CrashReport.java
+++ client/src/main/java/ru/wert/tubus/client/entity/models/CrashReport.java
@@ -9,12 +9,12 @@ import ru.wert.tubus.client.interfaces.Item;
 import ru.wert.tubus.client.entity.models._BaseEntity;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = {"id"}, callSuper = false)
 public class CrashReport extends _BaseEntity implements Item {
 
     private LocalDateTime date;
@@ -33,5 +33,18 @@ public class CrashReport extends _BaseEntity implements Item {
     public String toUsefulString() {
         return user + ": " + device + " v" + version + " - " +
                 (stackTrace != null ? stackTrace.substring(0, Math.min(stackTrace.length(), 50)) + "..." : "");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CrashReport)) return false;
+        CrashReport that = (CrashReport) o;
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
     }
 }
