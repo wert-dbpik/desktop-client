87387e5cd1e0e090ebcf7bbc6056d64b5cf359f3
Aleksei V. Simonov <wert001@yandex.ru>
2025-08-12 11:13:10 +0300

    add deleting crash reports

--- chogori/src/main/java/ru/wert/tubus/chogori/crashReport/CrashReport_TableView.java
+++ chogori/src/main/java/ru/wert/tubus/chogori/crashReport/CrashReport_TableView.java
@@ -1,5 +1,6 @@
 package ru.wert.tubus.chogori.crashReport;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,6 +8,7 @@ import javafx.scene.control.*;
 import ru.wert.tubus.chogori.common.contextMenuACC.FormView_ACCController;
 import ru.wert.tubus.chogori.common.tableView.ReadOnlyTableView;
 import ru.wert.tubus.client.entity.models.CrashReport;
+import ru.wert.tubus.winform.warnings.Warning2;
 
 import java.io.IOException;
 import java.util.List;
@@ -24,10 +26,24 @@ public class CrashReport_TableView extends ReadOnlyTableView<CrashReport> {
     public CrashReport_TableView(String itemName, boolean adminOnly, boolean useContextMenu) {
         super(itemName);
         this.adminOnly = adminOnly;
-        this.setEditable(true);
+        this.setEditable(false);
 
         if (useContextMenu)
             createContextMenu();
+
+        //По двойному клику открывается вкладка с отчетом
+        this.setRowFactory(tv -> {
+            TableRow<CrashReport> row = new TableRow<>();
+
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2 && !row.isEmpty()) {
+                    CrashReport clickedReport = row.getItem();
+                    openReportInTab(clickedReport);
+                }
+            });
+
+            return row;
+        });
     }
 
     @Override
@@ -46,29 +62,85 @@ public class CrashReport_TableView extends ReadOnlyTableView<CrashReport> {
     @Override
     public void createContextMenu() {
         ContextMenu contextMenu = new ContextMenu();
-        MenuItem deleteItem = new MenuItem("Удалить");
 
         MenuItem openItem = new MenuItem("Открыть отчет");
-        openItem.setOnAction(this::openReport);
+        openItem.setOnAction(this::openReports);
+
+        MenuItem deleteItem = new MenuItem("Удалить");
+        deleteItem.setOnAction(this::deleteReport);
+
         contextMenu.getItems().addAll(openItem, deleteItem);
         setContextMenu(contextMenu);
     }
 
-    private void openReport(ActionEvent actionEvent) {
+    private void deleteReport(ActionEvent actionEvent) {
+        // Получаем все выделенные отчеты
+        ObservableList<CrashReport> selectedReports = getSelectionModel().getSelectedItems();
+
+        if (selectedReports.isEmpty()) {
+            return;
+        }
+
+        // Запрашиваем подтверждение удаления
+        String message = selectedReports.size() > 1 ?
+                "Вы уверены, что хотите удалить " + selectedReports.size() + " выделенных отчетов?" :
+                "Вы уверены, что хотите удалить выделенный отчет?";
+
+        if (!Warning2.create("ВНИМАНИЕ!",
+                message,
+                "Отменить будет невозможно.")) {
+            return;
+        }
+
+        // Удаляем все выделенные отчеты
+
+        for (CrashReport report : selectedReports){
+            CH_CRASH_REPORTS.delete(report);
+        }
+
+        updateTableView();
+    }
+
+    /**
+     * Открывает группу выделенных в таблице отчетов в отдельных вкладках
+     * @param actionEvent, ActionEvent
+     */
+    private void openReports(ActionEvent actionEvent) {
+        // Получаем список всех выделенных элементов
+        ObservableList<CrashReport> selectedItems = getSelectionModel().getSelectedItems();
+
+        // Если ничего не выделено, выходим
+        if (selectedItems.isEmpty()) {
+            return;
+        }
+
+        // Для каждого выделенного элемента создаем вкладку
+        for (CrashReport item : selectedItems) {
+            openReportInTab(item);
+        }
+    }
+
+    /**
+     * Открывает отчет в новой вкладке
+     * @param item, CrashReport
+     */
+    private void openReportInTab(CrashReport item) {
         try {
-            String date = String.valueOf(getSelectionModel().getSelectedItem().getDate());
-            String report = getSelectionModel().getSelectedItem().getReport();
+            String date = String.valueOf(item.getDate());
+            String report = item.getReport();
+
             FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/chogori-fxml/crashReports/report.fxml"));
             Parent parent = loader.load();
             ((ReportController)loader.getController()).init(report);
             parent.getStylesheets().add(this.getClass().getResource("/chogori-css/details-dark.css").toString());
+
             String tabName = "crash: " + date;
             String tabId = tabName;
+
             CH_TAB_PANE.createNewTab(tabId, tabName, parent, true, loader.getController());
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     @Override
